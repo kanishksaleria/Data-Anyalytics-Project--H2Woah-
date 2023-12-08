6287aac801eabb59ae743bed86fe04f48d02e93d
Kedhar-K <kedhar.krisshnan@gmail.com>
2023-12-08 19:34:41 +0530

    Update views.py

--- data_analysis_project/data_analysis_app/views.py
+++ data_analysis_project/data_analysis_app/views.py
@@ -16,7 +16,15 @@ pa = ''
 
 def index(request):
     if(request.method == 'POST'):
-        if(request.post == '')
+        if(request.post == 'Himachal Pradesh'):
+            pa = ''
+        if(request.post == 'Uttarakhand'):
+            pa = ''
+        if(request.post == 'Rajasthan'):
+            pa = ''
+        if(request.post == 'Kerala'):
+            pa = ''
+    return render(request, 'cover.html', pa)
     
 
 def data_tab(request):
